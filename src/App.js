7dfe49939305ae2ce26697388a9226f1078dc298
Noi Sinnang <noisinnang@nois-imac.attlocal.net>
2022-03-13 23:56:57 -0700

    refactor from class base to functional

--- src/App.js
+++ src/App.js
@@ -4,18 +4,30 @@ import Navbar from './components/layout/Navbar';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
-import UserItem from './components/users/UserItem';
 import Users from './components/users/Users';
+import axios from 'axios';
 
 library.add(fab, faCheckSquare, faCoffee);
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const res = await axios.get('https://api.github.com/users');
+    console.log(res.data);
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar title="Noi's Github Finder" icon='fa-brands fa-github' />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export class Navbar extends Component {
-  // set default title and icon -- in case we forgot to setup props in App.js
-  static defaultProps = {
-    title: 'Github Finder',
-    icon: 'fa-brands fa-github',
-  };
+const Navbar = ({ title, icon }) => {
+  return (
+    <nav className='navbar bg-primary'>
+      <h1>
+        <FontAwesomeIcon icon={icon} /> {title}
+      </h1>
+    </nav>
+  );
+};
 
-  render() {
-    return (
-      <nav className='navbar bg-primary'>
-        <h1>
-          <FontAwesomeIcon icon={this.props.icon} /> {this.props.title}
-        </h1>
-      </nav>
-    );
-  }
-}
+// set default title and icon -- in case we forgot to setup props in App.js
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fa-brands fa-github',
+};
 
 export default Navbar;
